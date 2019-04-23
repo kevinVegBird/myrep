bd34b07c860d62945fc91e76df33ea467113516d
kevinVegBird <1421737226@qq.com>
2019-04-23 06:53:24 -0700

    test

---
+++ cppLearn/blackWhite.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int bw1(){
+    
+    int matrixLen = sizeof(matrix) / sizeof(matrix[0]);
+    int arrayLen = sizeof(metrix[0]) / sizeof(matrix[0][0]);
+    if (matrixLen <=2 ||arrayLen <= 2) return 0;
+    for(int i = 0; i<= matrixLen; i+=2){
+        for(int j =0; j<arrayLen; j++) {
+             
+        
+        }
+    }
+}
+int main(){
+
+
+
+}
